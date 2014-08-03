cacd3b2a6bd2e22cd714179e3d8ae2cfb4532581
Akira Osada <osd.akira@gmail.com>
2014-08-03 17:40:50 +0900

    tmp

---
+++ design/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+  char *hello = "Hello world!\n";
+  printf("%s", hello);
+}
--- design/tmp.c
+++ design/tmp.c
@@ -1,24 +1,4 @@
-#include <stdio.h>
-
-typedef struct{
-  int x;
-  int y;
-} point;
-
-int add(int a, int b){
-  return a + b;
-}
-
-point add_point(point a, point b){
-  point added_point;
-  added_point.x = add(a.x, b.x);
-  added_point.y = add(a.y, b.y);
-  return added_point;
-}
-
-void print_point(point p){
-  printf("x=%d, y=%d\n", p.x, p.y);
-}
+#include "point.c"
 
 int main(void){
   point point_1 = {1, 2};
